311554a0f31e55d8b179c4237eb1ebf918c68b84
AnuragRai <S10C-4484@vidyavihar.org>
2020-12-17 14:32:41 +0530

    Update snowfl token
    snowfl admin once again changed token

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   botToken: "<YOUR BOT TOKEN HERE>",
   parseMode: "HTML",
-  apiBaseUrl: "https://snowfl.com/OIcObqNfqpHTDvLKWQDNRlzQPbtqRcoKhtlled/",
+  apiBaseUrl: "https://snowfl.com/CtEtDPzcfBHDNVxjLqXuTCFNgtXAOJLWLzOQT/",
 };
